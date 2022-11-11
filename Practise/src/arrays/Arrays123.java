3f226887631fc7bcfd288b479ecd86b4abefb919
Vinayak <rajuvinayak06@gmail.com>
2022-11-11 23:12:27 +0530

    new files

--- Practise/src/arrays/Arrays123.java
+++ Practise/src/arrays/Arrays123.java
@@ -3,12 +3,13 @@ package arrays;
 public class Arrays123 {
 
 	public static void main(String[] args) {
-		int[] array = { 25, 26, 27, -5, -4, 32, -8, 60};
+		int[] array = {25,26,27, -5, -4, 32, -8, 60};
 
-		int i = 1;
+		int i = 1;  //it has taken second element
 		
 		int tempTotal = 0;
 		int total = array[0];
+		//System.out.println(array[0]);
 		for (int n : array) {
 			boolean isElse = false;
 			try {
@@ -16,16 +17,19 @@ public class Arrays123 {
 			} catch (ArrayIndexOutOfBoundsException e) {
 				break;
 			}
-			
+			//System.out.println(array[i]);
+		//	if (n >= 0 && array[i] >= 0 && (n + array[i+1]-array[i]) == array[i]) {
 			if (n >= 0 && array[i] >= 0 && (n + 1) == array[i]) {
 				
 				if (tempTotal != 0) n = tempTotal;
 				tempTotal = n + array[i];
 				
+				
 			} else {
 				isElse=true;
 				if (total < tempTotal) total = tempTotal;
 				tempTotal = 0;
+				//System.out.println(array[i]);
 				if (total < array[i]) total = array[i];
 				
 			}
@@ -33,7 +37,7 @@ public class Arrays123 {
 				
 			i++;
 		}
-
+		
 		System.out.println("greatest Number is = " + total);
 	}
 }
